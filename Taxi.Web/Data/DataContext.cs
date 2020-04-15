0ab924ef1050bb6468c5b928fd64347b81b0385d
German <gldeperosanz@gmail.com>
2020-04-14 21:23:03 -0300

    Upto video 10

--- Taxi.Web/Data/DataContext.cs
+++ Taxi.Web/Data/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Taxi.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Taxi.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<UserEntity>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -12,6 +13,16 @@ namespace Taxi.Web.Data
         public DbSet<TaxiEntity> Taxis { get; set; }
         public DbSet<TripEntity> Trips { get; set; }
         public DbSet<TripDetailEntity> TripDetails { get; set; }
+        public DbSet<UserGroupEntity> UserGroups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TaxiEntity>()
+                .HasIndex(t => t.Plaque)
+                .IsUnique();
+        }
 
     }
 }
